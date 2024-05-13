c70929bbf578965c6dea6d406fb98111c355375d
dileepkumar9972 <dileepkumar.s9972@gmail.com>
2024-05-13 18:46:29 +0530

    cra & tailwind

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500 text-center">
+      Welcome to NetflixGPT App
+    </div>
+  );
+}
+
+export default App;
